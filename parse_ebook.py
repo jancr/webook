2ce9426b12bf0f61f67089d7cc88d205bcb6ef03
jcr <jcr@intomics.com>
2017-11-23 15:22:58 +0100

    added tests for peputils.peputils, changed imports to be relative

--- parse_ebook.py
+++ parse_ebook.py
@@ -8,9 +8,9 @@ from webook.modules.wordpress import WordPressEBook
 
 def parse(args):
     if 'fanfiction.net' in args.url:
-        ebook = FanFictionEBook(args.url, args.book_file, args.title)
+        FanFictionEBook(args.url, args.book_file, args.title)
     else:  # assume wordpress
-        ebook = WordPressEBook(args.url, args.book_file, args.title)
+
 
 if __name__ == '__main__':
     example_url = "https://www.fanfiction.net/s/9658524/1/Branches-on-the-Tree-of-Time"
