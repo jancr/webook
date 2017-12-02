60bd8dfca4211736995375900c7037624f6297c9
jcr <jcr@intomics.com>
2017-12-02 20:05:20 +0100

    The webserver now has a working status bar and downloads the websites chapters cuncurently, but for some reason the file is not returned to the browser :'(

--- webook/runserver.py
+++ webook/runserver.py
@@ -7,14 +7,13 @@ import sys
 import hashlib
 import time
 import tempfile
+import base64
 
 # Setup for flask
 import flask
-from flask import render_template, request, send_file, after_this_request
-
+from flask import render_template, request, send_file, after_this_request, Response
 # local imports
-from webook.modules.fanfiction import FanFictionEBook
-from webook.modules.wordpress import WordPressEBook
+from webook.modules import FanFictionEBook, WordPressEBook
 
 app = flask.Flask(__name__, 
         template_folder='static/templates')
@@ -38,56 +37,89 @@ PARSER_MAPPERS = {'fanfiction.net': FanFictionEBook,
 ################################################################################
 # helpers
 ################################################################################
-def get_parser(parser_option):
+def get_parser(parser_option, url):
     if parser_option == 'auto detect':
+        if 'fanfiction.net' in url:
+            return FanFictionEBook
         raise NotImplemented
     else:
         parser_key = int(parser_option)
         return PARSER_MAPPERS[PARSERS[parser_key]]
 
 
+#  def ebook_wrapper(ebook, *args, **kwargs):
+    #  pass
+#  
+
 ################################################################################
 # html
 ################################################################################
 @app.route('/')
 def index():
-    print('test 1')
     return render_template('index.html', parsers=PARSERS)
 
 
-@app.route('/ebook')
-def make_ebook():
+#  @app.route('/download_ebook/<file_name>')
+@app.route('/download_ebook/<file_name>')
+def download_ebook(file_name):
+    """This function is called after progress, it simply returns 
+    the ebook and deletes it subsequently
+    """
+
     @after_this_request
     def remove_file(response):
-        tmp_file.close()
+        #  os.unlink(tmp_file_name)
         return response
 
-    book_parser = request.args['parser'] 
-    book_url = request.args['url']
+    tmp_file_name = pjoin(TMP_DIR, file_name)
+    print('\n==============')
+    print(tmp_file_name)
+    print('\n==============')
+
+    #  return flask.send_from_directory(TMP_DIR, tmp_file_name)
+    return send_file(tmp_file_name, mimetype='application/epub+zip')
+                     
+    #  return send_file(tmp_file_name, mimetype='application/epub+zip',
+                     #  attachment_filename="book.epub", as_attachment=True)
+
+@app.route('/create_ebook/<parser>/<url>')
+def create_ebook(parser, url):
+    """this function returns a progress bar while the book is
+    being made
+    """
     
-    tmp_file = tempfile.NamedTemporaryFile(dir=TMP_DIR, suffix='.epub')
+    def generate(ebook_parser, tmp_file):
+        ebook_generator = ebook_parser.run()
+        progress = next(ebook_generator)
+        total = ebook_parser.total
+        yield f"data: {round(100 * progress / total)}\n\n"
+        for progress in ebook_generator:
+            print(progress, total)
+            yield f"data: {round(100 * progress / total)}\n\n"
+        # hack: return file name for the next request
+        tmp_file = os.path.basename(tmp_file)
+        yield f"data: file-name: {tmp_file}\n\n"
 
-    ebook_parser = get_parser(book_parser)
-    ebook_parser(book_url, tmp_file.name)
+    book_parser = base64.b64decode(parser).decode('utf-8')
+    book_url = base64.b64decode(url).decode('utf-8')
 
-    return send_file(tmp_file.name, mimetype='application/epub+zip')
-
-@app.route('/progress')
-def progress():
-    raise NotImplemented
-    # TODO: 
-    #       1. make call back part of the api
-    #       2. use the call back here
-    #       3. use bootstrap to make it pretty
-
+    #  tmp_file = tempfile.NamedTemporaryFile(dir=TMP_DIR, suffix='.epub')
+    tmp_file = tempfile.mkstemp(dir=TMP_DIR, suffix='.epub')[1]
+    ebook_parser = get_parser(book_parser, url)
+    # todo, reduce number, this is a speed hack!!!
+    #  ebook_parser = ebook_parser(book_url, tmp_file, run=False, workers=20)
+    ebook_parser = ebook_parser(book_url, tmp_file, run=False)
+    print(f' == {tmp_file} == ')
+    return Response(generate(ebook_parser, tmp_file), mimetype='text/event-stream')
 
 def runserver(debugging=False, processes=4):
     global DEBUGGING
     DEBUGGING = debugging
     if debugging:
-        processes = 1
+        processes=1
     app.run(debug=debugging, host='0.0.0.0', port=5555, processes=processes)
 
 
 if __name__ == '__main__':
     runserver()
+
--- webook/static/webook.js
+++ webook/static/webook.js
@@ -1,17 +1,51 @@
+"use strict";
+
+////////////////////////////////////////////////////////////////////////////////
+// globals
+////////////////////////////////////////////////////////////////////////////////
+// var time_stamp = (new Date).getTime();
 
 ////////////////////////////////////////////////////////////////////////////////
 // Event functions
 ////////////////////////////////////////////////////////////////////////////////
-// var on_submit = (function(event) {
-    // var form = $("#ebook_form");
-    // var time = (new Date).getTime();
-    // form.attr("action", "/ebook/" + time);
-// });
+
+
+var submit_click = (function(event) {
+    event.preventDefault();
+    
+    // show progress bar and parse argd
+    $('.progress').show();
+    var url = btoa($("#url").val())
+    var parser = btoa($("#parser").val())
+
+    // var file_name = "tmplkmtr_j1.epub";
+    // var request_str = "/download_ebook/" + os.path.basename(file_name);
+    // $.get(request_str);
+    //
+    var progress = new EventSource("/create_ebook/" + parser + '/' + url);
+    progress.onmessage = function(event) {
+        // the last message is the file name that was created
+        if (/^file-name/.test(event.data)) {
+            var file_name = /^file-name: (.+)/.exec(event.data)[1]
+            var request_str = "/download_ebook/" + file_name;
+            $.get(request_str);
+            // $('#ebook_form').attr('action', 'download_ebook/' + file_name);
+            // $('#ebook_form').submit()
+
+            progress.close();
+        // all other messages are the progress in percent
+        } else {
+            $('.progress-bar').css('width', event.data+'%').attr('aria-valuenow', event.data);   
+        }
+    }
+});
 
 var example_click = (function(event) {
     var example = "https://www.fanfiction.net/s/10360716/1/The-Metropolitan-Man";
+    // this book is shorter
+    // var example = "https://www.fanfiction.net/s/12729710/1/Love-is-Love-Warrior-Cats-Fan-Fiction"
     $("#url").val(example);
-    $("#file_name").val('time');
+    // $("#file_name").val('time');
     $("#parser > option")[1].selected = true;
 });
 
@@ -19,8 +53,13 @@ var example_click = (function(event) {
 // Bind events
 ////////////////////////////////////////////////////////////////////////////////
 var load_index_page = (function() {
-    // $("#sumbit").click(on_submit);
     $("#example").click(example_click);
     // $(document).on('submit', '#ebook_form', on_submit);
+    $("#submit").click('submit', submit_click);
+    $('#url').bind("enterKey", submit_click);
+    //
+    // $("form").on("submit", function( event ) {
+        // event.preventDefault();
+    // });
 });
 
